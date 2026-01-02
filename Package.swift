d9d6deffa6aa7981d814690c21578d9e31f9b594
Ronit Sabhaya <ronitsabhaya75@gmail.com>
2026-01-02 14:28:22 -0600

    Fix duplicate swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
